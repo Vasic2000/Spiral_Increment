c371610e4346b1c526c6671d4563c314616d6208
Vasic2000 <Vasic2000@yandex.ru>
2019-04-18 15:51:37 +0300

    Теперь в обе стороны

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[][] matrix = Spiral_Increment(15);
+        int[][] matrix = Spiral_Increment_Clockwise(15);
+        printMatrix(matrix);
+
+        System.out.println();
+
+        matrix = Spiral_Increment_Counter_Clockwise(15);
         printMatrix(matrix);
 
     }
 
-    public static int[][] Spiral_Increment(int Size) {
+    public static int[][] Spiral_Increment_Clockwise(int Size) {
         int[][] matrix = new int[Size][Size];
         int x = 0;
         int y = 0;
@@ -59,6 +64,61 @@ public class Main {
         }
         return matrix;
     }
+
+
+    public static int[][] Spiral_Increment_Counter_Clockwise(int Size) {
+        int[][] matrix = new int[Size][Size];
+        int x = 0;
+        int y = 0;
+        int value = 1;
+        Direction dir = Direction.Down;
+        matrix[x][y] = value;
+
+        while (value < Size * Size) {
+            if (dir == Direction.Right) {
+                if ((x + 1) == matrix[0].length) {
+                    dir = Direction.Up;
+                } else if (matrix[x + 1][y] != 0) {
+                    dir = Direction.Up;
+                } else {
+                    x++;
+                    matrix[x][y] = ++value;
+                }
+            }
+            if (dir == Direction.Down) {
+                if ((y + 1) == matrix.length) {
+                    dir = Direction.Right;
+                } else if (matrix[x][y + 1] != 0) {
+                    dir = Direction.Right;
+                } else {
+                    y++;
+                    matrix[x][y] = ++value;
+                }
+            }
+            if (dir == Direction.Left) {
+                if ((x - 1) == -1) {
+                    dir = Direction.Down;
+                } else if (matrix[x - 1][y] != 0) {
+                    dir = Direction.Down;
+                } else {
+                    x--;
+                    matrix[x][y] = ++value;
+                }
+            }
+            if (dir == Direction.Up) {
+                if ((y - 1) == -1) {
+                    dir = Direction.Left;
+                } else if (matrix[x][y-1] != 0) {
+                    dir = Direction.Left;
+                } else {
+                    y--;
+                    matrix[x][y] = ++value;
+                }
+            }
+        }
+        return matrix;
+    }
+
 
     public static void printMatrix(int[][] matrix) {
         for(int j = 0; j < matrix.length; j++) {
